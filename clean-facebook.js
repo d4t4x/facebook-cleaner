4e7cb7f5182768113aa421b65a7e0f74dda092b1
Becca Ricks <baricks@gmail.com>
2018-02-08 00:38:47 -0500

    updates

--- clean-facebook.js
+++ clean-facebook.js
@@ -4,28 +4,63 @@
 
 var DEBUG = false;
 
+// First section - interests
 function cleanFacebookInterests(){
     tabs = $("._4xjz");
-    console.log(tabs);
+    // Click each tab
     for (i = 0; i < tabs.length; i++) {
       tabs[i].click();
-
+    // Click each like
       likes = $("._2b2n");
       for (y = 0; y < likes.length; y++) {
         likes[y].click();
       }
-      // TODO: Add 'See More' button
     };
+    sections = $("._2qo6");
+    sections[0].click();
+};
+
+// Second section - advertisers you've interacted with
+function cleanFacebookAds(){
+  sections = $("._2qo6");
+  sections[1].click();
+
+  tabs = $("._4jq5");
+  // Click each tab
+  for (f = 0; f < tabs.length; f++) {
+    tabs[f].click();
+  // Click each advert
+    adverts = $("._2b2n");
+    for (g = 0; g < adverts.length; g++) {
+      adverts[g].click();
+    }
+  };
+
+  more_tabs = $("._1b0");
+  sections[1].click();
+};
+
+// Third section - categories
+function cleanFacebookCategories(){
+  sections = $("._2qo6");
+  sections[2].click();
+
+  tabs = $("._4jq5");
+  tabs[1].click();
+
+  // Click each category
+  categories = $("._zom");
+  console.log(categories);
+  for (z = 0; z < categories.length; z++) {
+    categories[z].click();
+  }
 
 };
 
-function cleanFacebookAds(){
-}
-
 setTimeout(cleanFacebookInterests, 5000);
+// setTimeout(cleanFacebookAds, 10000);
+setTimeout(cleanFacebookCategories, 5000);
 
-setTimeout(cleanFacebookAds, 5000);
 
-//
 // var scroller = _.debounce(cleanFacebookAds, 300);
 // document.addEventListener("scroll", scroller);
